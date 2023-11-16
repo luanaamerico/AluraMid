483476df199e0b4dbc4ca79fc31fed30e76ab43e
luana americo de oliveira <103039954+luanaamerico@users.noreply.github.com>
2023-11-16 09:38:55 -0300

    .

--- main.js
+++ main.js
@@ -7,12 +7,16 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0;
 
 while(contador < 9) {
-     listaDeTeclas[contador].onclik = function(){
-        tocaSom('#som_tecla_clap')
+
+const instrumento = listaDeTeclas[contador].classList[1];
+
+const tecla = listaDeTeclas[contador];
+
+const idAudio = `#som_${instrumento}`;
+
+     tecla.onclik = function(){
+        tocaSom(idAudio)
      };
      contador = contador + 1;
 }
-listaDeTeclas[1].onclik = tocaSomClap;
-listaDeTeclas[2].onclik = tocaSomTim;
-listaDeTeclas[3].onclik = tocaSomPuff;
-document.querySelector('.tecla_pom').onclik=tocaSomPom();
+
